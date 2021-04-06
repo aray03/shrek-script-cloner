ebfa7fd7704ce67b861c10b8a78c9c949f7115f3
aray03 <aidan@andersenweb.com>
2021-04-06 12:54:04 -0600

    Optimized the Scripts a TON! And made it so the %s are cleaner

--- SHREKDESTROY.py
+++ SHREKDESTROY.py
@@ -2,16 +2,23 @@ import os
 stall=input("\n\n\n\n\n\n\n\n<><><> Press Enter To Begin <><><>")
 amount=input("How many copies do we need to remove?: ")
 
+count = 0
+newNum = 0
 amount=int(amount)
 for num in range(amount):
 
     try:
         os.remove('shrek' +str(num) + '.txt')
-        os.system('cls' if os.name == 'nt' else 'clear')
+        count = count+1
     except FileNotFoundError:
-        os.system('cls' if os.name == 'nt' else 'clear')
-    print(int(num/amount*1000)/(amount/100), "%"  )
+       count = count
+
+    #The print statement to make sure we are not repeating %s
+    if int((num/amount)*10000)/100 > newNum:
+        print(str(int((num/amount)*10000)/100) + "%")
+        newNum = int((num/amount)*10000)/100
 
 os.system('cls' if os.name == 'nt' else 'clear')
-print("100 %")
+print("100%")
+print("Deleted", count, "Files")
 print("The Work Has Been Finished!")
